57268aef648d3df53bcee0a7053025a8ec9f7109
Salman Ansari <salman.ansari@gmail.com>
2014-07-01 15:44:19 +0200

    added validations and styling

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,24 +1,32 @@
 class TweetsController < ApplicationController
+
   def index
     @tweets = Tweet.all
   end
 
   def new
     @tweet = Tweet.new
-    @handles = {}
-    User.all.each { |user| @handles[user.handle] = user.id }
+    set_handles
   end
 
   def create
-    # create a tweet
-    Tweet.create(tweet_params)
-
-    redirect_to :action => :index
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      redirect_to :action => :index  
+    else
+      set_handles
+      render :new
+    end
   end
 
   private
 
   def tweet_params
     params.require(:tweet).permit(:body, :user_id)
+  end
+
+  def set_handles
+    @handles = {}
+    User.all.each { |user| @handles[user.handle] = user.id }
   end
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,5 @@
 class Tweet < ActiveRecord::Base
   belongs_to :user
+
+  validates :body, presence: true, length: { maximum: 140 }
 end
